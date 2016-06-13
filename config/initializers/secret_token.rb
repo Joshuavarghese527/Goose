1d4cd10976046be8996efc5c506996faff35d17e
Joshua Varghese <joshuavarghese527@gmail.com>
2016-06-13 19:34:58 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Goose::Application.config.secret_key_base = '1ac8007e58ae8310fbdc766b2ea614ecd294fa44c47bf9dd8778bf43b4fc307585d688193504f3c0df644ea965cb6e474f71cda0a37e2466971dd0c677480057'
